320d70fa9d1e85e38203b28c903b7d93c524e162
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-06-28 19:01:05 +0200

    Update python_terminal_frame.py

--- python_terminal_frame.py
+++ python_terminal_frame.py
@@ -4,6 +4,8 @@ interface_width = 50
 previous_page = ""
 current_page = "main"
 exit_program = False
+s = ""
+COM_port = ""
 
 # UTIL FUNCTIONS
 def clear():
@@ -20,8 +22,18 @@ def pick_option(options = [], exit_option = False, return_option = False):
         print("| "+option_string.ljust(interface_width-4)+" |")
     return options
 
-def select():
-    return input('| > ')
+def select(options):
+    valid_option = False
+    while not valid_option:
+        i = input('| > ').strip()
+        for option in options:
+            if option[0] == i:
+                valid_option = True
+                break
+        if not valid_option:
+            print("Please enter a valid option...")
+    return i
+    
 
 def close_program():
     exit_program = True
@@ -42,8 +54,9 @@ def find_arduino_port():
 def list_COM_ports():
     pass
 def manually_select_COM():
-    pass
-
+    # Ask user to enter a COM port
+    return "COM5"
+    
 # MAIN FUNCTIONS
 def main_menu():
     bar()
@@ -55,13 +68,28 @@ def main_menu():
         ['S', 'Manually select COM', manually_select_COM]
     ], exit_option=True)
     bar()
-    s = select()
-    clear()
+    s = select(options)
+    if s == "s":
+        find_arduino_port()
+    elif s == "l":
+        list_COM_ports()
+    elif s == "S":
+        COM = manually_select_COM()
+    else:
+        return s
+    if COM != "":
+        next_page("main_connected")
 
 
 # MAIN FUNCTION
 while not exit_program:
-    if current_page("main"):
-        main_menu()
+    clear()
+    if current_page == "main":
+        s = main_menu()
     else:
-        exit_program = False
+        exit_program = True
+
+    if s == "e":
+        exit_program = True
+    elif s == "r":
+        return_to_previous()
